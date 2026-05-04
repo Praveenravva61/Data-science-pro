62ba506f26763693562af78f11838fcc5596d991
Praveen.ravva61 <praveen.ravva61@gmail.com>
2026-05-04 12:04:03 +0530

    updated readme file

---
+++ README.md
@@ -0,0 +1,20 @@
+# End to End Data science Project
+
+## Workflow -- ML_pipeline
+
+1. Data Ingestion
+2. Data validation
+3. Data Transformation
+4. Model Training
+5. Model Evaluation
+
+## Workflows
+
+1. update config.yaml 
+2. update schema.yaml
+3. update params.yaml
+4. update entity.yaml
+5. update configuraion manager in src config
+6. update the components
+7. update pipeline
+8. update main.py
--- src/DataSciencePro/utils/common.py
+++ src/DataSciencePro/utils/common.py
@@ -38,3 +38,68 @@ def read_yaml(path_to_yaml: Path)-> ConfigBox:
         raise e
     
     
+@ensure_annotations
+def create_dictionary(path_to_dictionaries: path, verbose= True)-> dict:
+    """ 
+
+    Args:
+        path_to_dictionaries (path): _description_
+        verbose (bool, optional): _description_. Defaults to True.
+        
+    """
+    for path in path_to_dictionaries:
+        os.makedirs(path, exist_ok= True)
+        if verbose:
+            logger.info(f"created directory at {path}")
+
+  
+    
+    
+@ensure_annotations
+def save_json(path: Path, data: dict)-> None:
+    """
+    save json data
+    Arg:
+        path: path to json file
+        data: dict data to be saved in json file
+    """
+    with open(path, "w") as f:
+        json.dump(data, f, indent= 4)
+        
+    logger.info(f"json file is saved at {path}")
+    
+@ensure_annotations
+def load_json(path: Path)-> dict:
+    
+    """
+    load json file data
+    
+    ARGS: path to json file
+    
+    Returns: 
+         ConfigBox : data as class attributes instead of dict
+    
+    """
+    
+    with open(path, 'r') as f:
+        content= json.load(f)
+        logger.info(f"json file is loaded from {path}")
+        return ConfigBox(content)
+    
+    
+@ensure_annotations
+def save_bin(path: Path, data: Any):
+    joblib.dump(data, path)
+    logger.info(f"binary file is saved at {path}")
+    
+@ensure_annotations
+def load_bin(path: Path)-> Any:
+    data= joblib.load(path)
+    logger.info(f"binary file is loaded from {path}")
+    return data
+ 
+    
+ 
+
+    
+    
